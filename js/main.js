51352687637d806099b612b68b4c27b3a60b2990
omar magdy abdelmoneam <47141593+omarmagdy99@users.noreply.github.com>
2022-11-15 22:04:33 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,51 @@
+//search message 1
+function cID(){
+  let cCid=document.getElementById("cCid").value;
+  
+  // window.alert(cCid.length)
+  if(cCid.length==9 ){
+    window.open(`https://sniffer.global-bilgi.com.tr/379/Monitoring#/q=id:${cCid}`, "_blank",)
+    
+  }else{
+    window.alert('pleas Enter the ID')
+  }
+  
+}
+
+//search message 2
+function cID2(){
+  let cCid2=document.getElementById("cCid2").value;
+
+  if(cCid2.length==9 ){
+    window.open(`https://sniffer.global-bilgi.com.tr/379/Monitoring#/q=id:${cCid2}`, "_blank",)
+    
+  }else{
+    window.alert('pleas Enter the ID')
+}
+}
+
+
+//search user
+function sID(){
+  let sSid=document.getElementById("sSid").value;
+
+  if(sSid.length==16 ){
+    window.open(`https://sniffer.global-bilgi.com.tr/379/Monitoring#/&nq=fromuserid:${sSid}`, "_blank",)
+    
+  }else{
+    window.alert('pleas Enter the ID')
+}
+}
+
+
+
+//password
+function smPassword(){
+  let txtPassword=document.getElementById("exampleInputPassword1").value;
+  if(txtPassword == 123456){
+    document.getElementById("smPassword").remove();
+  }else{
+    document.getElementById('smPError').innerHTML += `<div class="alert alert-danger" role="alert">the Password is wrong!</div>`   ;
+  }
+}
+
